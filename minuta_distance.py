5ca66b7f4f7e14024ba63247d086b16676ab8202
hornt <horntx3@gmail.com>
2024-11-16 20:34:17 -0500

    Starting files

---
+++ minuta_distance.py
@@ -0,0 +1,6 @@
+## Method 1 - Minuta distance
+## This method attempts to extract the minuta from an image, then it multiplies the distance between extracted minuta together
+## the product of the distances is the fingerprints ID and is stored for later comparison.
+
+
+
